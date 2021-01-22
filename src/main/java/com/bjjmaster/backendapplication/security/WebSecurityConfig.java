f0449b4b38ce3e61c58770a44c17cd872c0b3d97
Evangelos Vatikiotis <evangelos.vatikiotis@capgemini.com>
2021-01-23 00:48:32 +0100

    password encoding and way to populate data in database

--- src/main/java/com/bjjmaster/backendapplication/security/WebSecurityConfig.java
+++ src/main/java/com/bjjmaster/backendapplication/security/WebSecurityConfig.java
@@ -1,16 +1,23 @@
 package com.bjjmaster.backendapplication.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/bjjmaster/backendapplication/service/impl/AppUserServiceImpl.java
+++ src/main/java/com/bjjmaster/backendapplication/service/impl/AppUserServiceImpl.java
@@ -6,6 +6,7 @@ import com.bjjmaster.backendapplication.repository.AppUserRepository;
 import com.bjjmaster.backendapplication.service.AppUserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,16 +14,20 @@ public class AppUserServiceImpl implements AppUserService {
 
     private ModelMapper modelMapper;
     private AppUserRepository appUserRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     public AppUserServiceImpl(ModelMapper modelMapper,
-                              AppUserRepository appUserRepository) {
+                              AppUserRepository appUserRepository,
+                              PasswordEncoder passwordEncoder) {
         this.modelMapper = modelMapper;
         this.appUserRepository = appUserRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public AppUser registerAppUser(RegisterUserDTO registerUserDTO) {
+        registerUserDTO.setPassword(passwordEncoder.encode(registerUserDTO.getPassword()));
         AppUser appUser = modelMapper.map(registerUserDTO, AppUser.class);
         appUserRepository.save(appUser);
         return appUser;
