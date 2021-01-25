fefaee1d852414b4ee9eaf45b7ef72414e1d5de2
Evangelos Vatikiotis <evangelos.vatikiotis@capgemini.com>
2021-01-25 22:01:29 +0100

    populate passwords in database initialization script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO app_user (id, email, password, username)
 VALUES
-    (1, 'richie@bjjmaster.com', '1234', 'monster'),
-    (2, 'fabian@bjjmaster.com', '1234', 'purplegeek'),
-    (3, 'fran@bjjmaster.com', '1234', 'professor');
+    (1, 'richie@bjjmaster.com', '$2a$12$2Effzu3VNhZppkGJEhVACuJw4PdeXcJVQOpC91ZMuvM1qlbIRZdbK', 'monster'),
+    (2, 'fabian@bjjmaster.com', '$2a$12$2Effzu3VNhZppkGJEhVACuJw4PdeXcJVQOpC91ZMuvM1qlbIRZdbK', 'purplegeek'),
+    (3, 'fran@bjjmaster.com', '$2a$12$2Effzu3VNhZppkGJEhVACuJw4PdeXcJVQOpC91ZMuvM1qlbIRZdbK', 'professor');
